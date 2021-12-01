8a5553f274d2ac37aea8204d243ef108c41fe80e
Jan Nagel <j.nagel@neogramm.de>
2021-12-01 10:54:35 +0100

    added Day 1 Part 2

--- AdventOfCode2021/Day1.cs
+++ AdventOfCode2021/Day1.cs
@@ -7,26 +7,45 @@ namespace AdventOfCode2021
     {
         static void Main(string[] args)
         {
-            int counter = 0;
+            var counter = 0;
+            var counterThreeMeasurePoints = 0;
 
-            // Read the file and display it line by line.
+            // Part 1
             var filepath = Path.Combine(Directory.GetCurrentDirectory(), "input01.txt");
-            var file = File.ReadLines(filepath);
+            string[] lines = File.ReadAllLines(filepath);
 
             var oldvalue = 0;
-            foreach (string line in file)
+            foreach (string line in lines)
             {
                 var value = Convert.ToInt32(line);
                 if (value > oldvalue)
                 {
-                    counter++;
+                    if (!(oldvalue == 0))
+                    {
+                        counter++;
+                    }
                 }
-                Console.WriteLine(line);
-
                 oldvalue = value;
             }
+            Console.WriteLine($"There were {counter} increases with single measure point comparison.");
 
-            Console.WriteLine("There were {0} increases.", counter - 1);  
+
+            // Part 2
+            oldvalue = 0;
+            for (int i = 0; i < lines.Length - 2; i++)
+            {
+                var value = Convert.ToInt32(lines[i]) + Convert.ToInt32(lines[i + 1]) + Convert.ToInt32(lines[i + 2]);
+                if (value > oldvalue)
+                {
+                    if (!(oldvalue == 0))
+                    {
+                        counterThreeMeasurePoints++;
+                    }
+                }
+                oldvalue = value;
+            }
+            Console.WriteLine($"There were {counterThreeMeasurePoints} increases with three measure point comparison.");
+
             Console.ReadLine();
         }
     }
